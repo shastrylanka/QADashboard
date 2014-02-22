c1c61a4fe3c7e0397064b9c0da43ac6c3ca1518f
Shastry Lanka <lshastry@yahoo-inc.com>
2014-02-22 11:37:31 -0800

    Added files related to structs hibernate

---
+++ src/com/yahoo/qadashboard/controller/ContactManager.java
@@ -0,0 +1,47 @@
+package com.yahoo.qadashboard.controller;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.yahoo.qadashboard.model.Contact;
+import com.yahoo.qadashboard.util.HibernateUtil;
+ 
+public class ContactManager extends HibernateUtil {
+ 
+    public Contact add(Contact contact) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        session.save(contact);
+        session.getTransaction().commit();
+        return contact;
+    }
+    public Contact delete(Long id) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Contact contact = (Contact) session.load(Contact.class, id);
+        if(null != contact) {
+            session.delete(contact);
+        }
+        session.getTransaction().commit();
+        return contact;
+    }
+ 
+    public List<Contact> list() {
+         
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        List<Contact> contacts = null;
+        try {
+             
+            contacts = (List<Contact>)session.createQuery("from Contact").list();
+             
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        session.getTransaction().commit();
+        return contacts;
+    }
+}
